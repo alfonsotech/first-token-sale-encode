c1283d5c347aeacf3b6048ba189da3967a078fdd
Alfonso Tech <alfonsoteched@nym.hush.com>
2024-08-21 14:09:51 -0400

    Mints MyToken

--- scripts/TestTokens.ts
+++ scripts/TestTokens.ts
@@ -14,34 +14,39 @@ async function main() {
 
     // Fetching the role code
     const code = await tokenContract.read.MINTER_ROLE();
-    const mintTx = await tokenContract.write.mint(
-        [deployer.account.address, parseEther("10")],
-        { account: account2.account }
-    );
-    await publicClient.waitForTransactionReceipt({ hash: mintTx });
+    // const mintTx = await tokenContract.write.mint(
+    //     [deployer.account.address, parseEther("10")],
+    //     { account: account2.account }
+    // );
+    // await publicClient.waitForTransactionReceipt({ hash: mintTx });
 
     // Giving role
-    // const roleTx = await tokenContract.write.grantRole([
-    //   code,
-    //   account2.account.address,
-    // ]);
-    // await publicClient.waitForTransactionReceipt({ hash: roleTx });
+    const roleTx = await tokenContract.write.grantRole([
+      code,
+      account2.account.address,
+    ]);
+    await publicClient.waitForTransactionReceipt({ hash: roleTx });
+
+    //Minting Tokens
+    const mintTx = await tokenContract.write.mint([deployer.account.address, parseEther("10")], {account: account2.account});
+    await publicClient.waitForTransactionReceipt({hash:mintTx});
+
 
     //Fetching token data with Promise.all()
-    // const [name, symbol, decimals, updatedTotalSupply] = await Promise.all([
-    //   tokenContract.read.name(),
-    //   tokenContract.read.symbol(),
-    //   tokenContract.read.decimals(),
-    //   tokenContract.read.totalSupply(),
-    // ]);
-    // console.log({ name, symbol, decimals, totalSupply: updatedTotalSupply });
+    const [name, symbol, decimals, updatedTotalSupply] = await Promise.all([
+      tokenContract.read.name(),
+      tokenContract.read.symbol(),
+      tokenContract.read.decimals(),
+      tokenContract.read.totalSupply(),
+    ]);
+    console.log({ name, symbol, decimals, totalSupply: updatedTotalSupply });
 
     // Sending a transaction
-    // const tx = await tokenContract.write.transfer([
-    //   account1.account.address,
-    //   parseEther("2"),
-    // ]);
-    // await publicClient.waitForTransactionReceipt({ hash: tx });
+    const tx = await tokenContract.write.transfer([
+      account1.account.address,
+      parseEther("2"),
+    ]);
+    await publicClient.waitForTransactionReceipt({ hash: tx });
 }
 
 main().catch((err) => {
